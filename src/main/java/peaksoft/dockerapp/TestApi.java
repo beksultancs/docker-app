5117993db31455401677864d612b8445895237f4
beksultan <beksultancs@gmail.com>
2022-05-02 21:09:50 +0600

    EB DEPLOY COMMIT

---
+++ src/main/java/peaksoft/dockerapp/TestApi.java
@@ -0,0 +1,39 @@
+package peaksoft.dockerapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author Beksultan
+ */
+@RestController
+@ConfigurationProperties(prefix = "test")
+public class TestApi {
+
+    private String activeProfile;
+
+    @GetMapping
+    public Map<String, String> isWorking() {
+        return Map.of(
+                "WORKING", "true"
+        );
+    }
+
+    @GetMapping("active/profile")
+    public Map<String, String> showActiveProfile() {
+        return Map.of(
+                "SPRING_PROFILES_ACTIVE", activeProfile
+        );
+    }
+
+    public String getActiveProfile() {
+        return activeProfile;
+    }
+
+    public void setActiveProfile(String activeProfile) {
+        this.activeProfile = activeProfile;
+    }
+}
